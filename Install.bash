24856a96cb828b86541be1a1d6f25c5cdaeadbf6
michael noble <michaelhnoble@gmail.com>
2026-02-22 06:38:57 -0500

    u

--- Install.bash
+++ Install.bash
@@ -1,2 +1,13 @@
 #!/bin/bash
 #
+sudo dnf install -y nginx
+sudo systemctl enable --now nginx
+
+sudo dnf install -y httpd-tools
+sudo mkdir -p /var/www/html /var/www/logs /var/www/dynatrace /var/www/misc /var/www/api
+sudo chown -R nginx:nginx /var/www
+sudo chmod -R 755 /var/www
+
+TOKEN="$(openssl rand -hex 32)" ;echo "YOUR_TOKEN=$TOKEN"
+sudo htpasswd -c /etc/nginx/.htpasswd portaluser
+
